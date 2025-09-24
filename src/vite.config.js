bfcfda62969cba33fb990d78d36a47e4fef8c3e3
11happysingh11 <66782508+11happysingh11@users.noreply.github.com>
2025-09-25 02:38:58 +0530

    Update vite.config.js
    all allowwed

--- src/vite.config.js
+++ src/vite.config.js
@@ -2,8 +2,8 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   preview: {
-    port: 10000,
+    port: process.env.PORT || 10000,
     host: "0.0.0.0",
-    allowedHosts: ["pravaah.onrender.com"]
+    allowedHosts: ["*"]
   }
 });
